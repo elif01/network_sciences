564f944970c9288989988fc367850bce183b6569
Ana Arsovska <anamarsovska@gmail.com>
2020-12-13 20:09:10 +0100

    latest code used in slides, dataset of 10k artists

--- similarity.py
+++ similarity.py
@@ -5,6 +5,8 @@ import spotipy
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import random
+import matplotlib.pyplot as plt
+
 
 ## TO DO: HOW WILL YOU MAKE IT UNIVERSAL ##
 PORT_NUMBER = 8080
@@ -13,10 +15,17 @@ SPOTIPY_CLIENT_SECRET = '1fe80ea9b0ce464aab9afbb51fbc7800'
 SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
 
 mosdef_uri = "spotify:artist:0Mz5XE0kb1GBnbLQm2VbcO"
+taylor_swift_uri = "spotify:artist:06HL4z0CvFAxyc27GXpf02"
+johnny_cash_uri = "spotify:artist:6kACVPfCOnqzgfEF5ryl0x"
+kanye_uri = "spotify:artist:5K4W6rqBFWDnAN6FQUkS6x"
+meg_thee_uri = "spotify:artist:181bsRPaVXVlUKXrxwZfHK"
+phoebe_bridgers_uri = "spotify:artist:1r1uxoy19fzMxunt3ONAkG"
+beatles_uri = "spotify:artist:3WrFJ7ztbogyGnTHbHJFl2"
+
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 artists = []
-f = open("filtered.txt")
+f = open("filtered_artists.txt")
 
 for line in f:
     artists.append(line.rstrip())
@@ -30,42 +39,50 @@ def get_id_from_name(name):
     url = items["uri"]
     return(url) 
 
+#takes in ID and returns list of genres for that artist
 def get_genre(artist_id):
     artist_object = spotify.artist(artist_id)
     genres = artist_object["genres"]
-    print(genres)
     return(genres) 
 
-# full list of related artist
+#takes in ID and returns full list of related artist
 def get_related_artists(artist_id):
     artists = spotify.artist_related_artists(artist_id)['artists']
     ans = [x['uri'] for x in artists]
     return ans
 
-def is_related_to(random_artist, target_artist):
-    target_related = get_related_artists(target_artist)
-    return (random_artist in target_related)
+#takes in two artist IDs and returns true if related and false if not
+def are_related(random_artist_id, target_artist_id):
+    random_related = get_related_artists(random_artist_id)
+    target_related = get_related_artists(target_artist_id)
+    return (random_artist_id in target_related or target_artist_id in random_related)
 
+#takes artist ID and returns list of five related artists
 def five_related(artist_id):
     return get_related_artists(artist_id)[:5]
 
+#gets the whole artist list and sample size n, and returns a sample of n artists
 def sample(artist_list, n):
     sample = random.sample(artist_list, n)
     sample_id = []
     for s in sample:
-        sample_id.append(get_id_from_name(s))
+        sample_id.append(s)
 
     return sample_id
 
-def contest_design(popular_artists, target_artist):
-    random_artists_45 = sample(popular_artists, 200)
+#returns a list of 5 related artists and n unrelated artists
+def contest_design(artist_list, target_artist, n):
+    random_artists = sample(artist_list, n)
     related_artists_5 = five_related(target_artist)
-    random_artists_45.extend(related_artists_5)
-    return random_artists_45
+    random_artists.extend(related_artists_5)
+    return random_artists
 
-def genre_similarity_scores(target_artist, artist_test_list):
+#takes in target artist, the list of all artists, and a sample size n
+#returns a similarity score as a dictionary, between the artist and 
+#all of the other artists in the sample
+def genre_similarity_scores(target_artist, artist_list, n):
     target_genres = get_genre(target_artist)
-    other_artists = contest_design(artist_test_list, target_artist)
+    other_artists = contest_design(artist_list, target_artist, n)
 
     similarity_score = {}
     for artist in other_artists:
@@ -79,14 +96,75 @@ def genre_similarity_scores(target_artist, artist_test_list):
 
     return similarity_score
 
-score = genre_similarity_scores(mosdef_uri, artists)
+#takes the similarity score dictionary returned by genre similarity scores
+#and returns the top 5 highest scoring artists
+def get_top_five_similar_artists(similarity_score):
+    sorted_similar = sorted(similarity_score.items(), key=lambda x: x[1], reverse=True)
+    sorted_similar = sorted_similar[:5]
+    top_5 = []
+    for k,v in sorted_similar:
+        top_5.append(k)
 
-def visualize_similarity(target, similarity_score):
+    return top_5
+
+#compares the prediction top 5 with the actual top 5 and returns a score
+def scoring_comp(target_artist, artist_list, similarity_score):
+    top_five = get_top_five_similar_artists(similarity_score)
+    real_top_five = five_related(target_artist)
+
+    score = 0
+
+    for a in top_five:
+        if(a in real_top_five):
+            score += 1
+
+    return score
+
+#takes in a target artist, list of all artists and a similarity score dictionary
+#constructs a graph wherein the target node is black, the spotify related artists are red
+#and the remainder are blue. returns a graph with edges between target node and nodes
+#we predict are related
+def visualize_similarity_guessed(target, artists, similarity_score):
     graph = nx.Graph()
+    color_map = []
 
-    for k,v in similarity_score:
-        graph.add_edge(target, k, weight=v)
+    related = five_related(target)
 
-    draw_circular(graph)
+    graph.add_node(target)
+    graph.add_nodes_from(artists)
 
-visualize_similarity(mosdef_uri, score)
+    for k,v in similarity_score.items():
+        if(v > 0):
+            graph.add_edge(target, k, weight=v, length = 10)
+
+    for node in graph.nodes():
+        if(node in related):
+            color_map.append('red')
+        elif(node == target):
+            color_map.append('black')
+        else:
+            color_map.append('blue')
+
+    nx.draw(graph, node_color=color_map, node_size = 1)
+    plt.show()
+
+
+# similarity_score = genre_similarity_scores(kanye_uri, artists, 100)
+# visualize_similarity_guessed(kanye_uri, artists, similarity_score)
+
+
+#scores how our function performs with various sample sizes and plots it
+sample_size = [50, 250, 500, 1000]
+scores_ye = []
+for i in sample_size:
+    similarity_score = genre_similarity_scores(mosdef_uri, artists, i-5)
+    scores_ye.append(scoring_comp(mosdef_uri, artists, similarity_score))
+
+plt.plot(sample_size, scores_ye)
+plt.title('Mos Def - Similarity scoring')
+plt.xlabel('Sample size')
+plt.ylabel('Correctly guessed similar artists')
+plt.show()
+
+
+
